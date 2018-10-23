c781c6f892512f6aefbf13912dd1cb9b3d80b897
huadong <hd2260332898@gmail.com>
2018-10-23 11:26:38 +0800

    first commit

---
+++ src/main/java/com/ys/redis/RedisApplication.java
@@ -0,0 +1,16 @@
+package com.ys.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author HD
+ * @date 2018/10/23 11:20
+ */
+@SpringBootApplication
+public class RedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisApplication.class, args);
+    }
+}
